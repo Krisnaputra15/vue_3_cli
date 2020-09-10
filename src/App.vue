cc4e5f0f13bb62e4ff0c92f51a74b4c34759efc5
krisnaputra15 <krisnaputra1530@gmail.com>
2020-09-10 14:53:06 +0700

    vue spa 3 done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <first msg="first component"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import first from './components/first.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    first
   }
 }
 </script>
